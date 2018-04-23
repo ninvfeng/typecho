47c29740bda3daf6d4240a83b893b3ce6b92508e
冯誉信 <ninvfeng@qq.com>
2018-04-23 16:46:06 +0800

    update article img

--- usr/themes/Kratos/functions.php
+++ usr/themes/Kratos/functions.php
@@ -3,16 +3,16 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 /* 后台设置 */
 function themeConfig($form) {
-	//header部分
+    //header部分
     $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点LOGO地址'), _t('在这里填入一个图片URL地址, 以在网站标题前加上一个LOGO'));
-	$form->addInput($logoUrl->addRule('xssCheck', _t('请不要在图片链接中使用特殊字符')));
-	$logoTxt = new Typecho_Widget_Helper_Form_Element_Text('logoTxt', NULL, NULL, _t('居中标题'), _t('Banner正中展示的简单标题'));
-	$form->addInput($logoTxt);
+    $form->addInput($logoUrl->addRule('xssCheck', _t('请不要在图片链接中使用特殊字符')));
+    $logoTxt = new Typecho_Widget_Helper_Form_Element_Text('logoTxt', NULL, NULL, _t('居中标题'), _t('Banner正中展示的简单标题'));
+    $form->addInput($logoTxt);
 
-	$bannerimg = new Typecho_Widget_Helper_Form_Element_Text('bannerimg', NULL, NULL, _t('顶部Banner图片'), _t('顶部Banner图片链接'));
+    $bannerimg = new Typecho_Widget_Helper_Form_Element_Text('bannerimg', NULL, NULL, _t('顶部Banner图片'), _t('顶部Banner图片链接'));
     $form->addInput($bannerimg);
 
-	$site_bw = new Typecho_Widget_Helper_Form_Element_Radio('site_bw',
+    $site_bw = new Typecho_Widget_Helper_Form_Element_Radio('site_bw',
         array('able'=>_t('开启'),'disable'=>_t('关闭')),
         'disable',
         _t("站点黑白模式"),
@@ -20,57 +20,57 @@ function themeConfig($form) {
         );
     $form->addInput($site_bw);
 
-	
-	//侧边栏
-	$sidebarlr = new Typecho_Widget_Helper_Form_Element_Radio('sidebarlr',
+    
+    //侧边栏
+    $sidebarlr = new Typecho_Widget_Helper_Form_Element_Radio('sidebarlr',
         array('left_side' => _t('左边栏'),
             'right_side' => _t('右边栏'),
-			'single' => _t('无边栏'),
+            'single' => _t('无边栏'),
         ),
         'right_side', _t('非首页侧边栏设置'), _t('默认右边栏'));
     $form->addInput($sidebarlr);
-	
+    
     $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', 
     array('ShowAuthor' => _t('作者简介'),
     'ShowSearch' => _t('搜索框'),
     'ShowRecent' => _t('最新文章'),
-	'ShowCategory' => _t('分类目录'),
-	'ShowTags' => _t('标签云')),
+    'ShowCategory' => _t('分类目录'),
+    'ShowTags' => _t('标签云')),
     array('ShowAuthor', 'ShowSearch', 'ShowRecent', 'ShowCategory', 'ShowTags'), _t('侧边栏显示'));    
     $form->addInput($sidebarBlock->multiMode());
-	
-	$authordesc = new Typecho_Widget_Helper_Form_Element_Text('authordesc', null, NULL, _t('作者简介'), _t('侧边栏的作者简介以&lt;br&gt;换行 图片请自行替换images文件夹中about.jpg和author.jpg 或者自行修改sidebar.php'));
-	$form->addInput($authordesc);
+    
+    $authordesc = new Typecho_Widget_Helper_Form_Element_Text('authordesc', null, NULL, _t('作者简介'), _t('侧边栏的作者简介以&lt;br&gt;换行 图片请自行替换images文件夹中about.jpg和author.jpg 或者自行修改sidebar.php'));
+    $form->addInput($authordesc);
 
 
 
-	//社交
+    //社交
     $socialweixin = new Typecho_Widget_Helper_Form_Element_Text('socialweixin', NULL, NULL, _t('输入微信二维码链接'), _t('在这里输入微信二维码链接,图片格式,支持 http:// 或 https:// 或 //'));
     $form->addInput($socialweixin->addRule('xssCheck', _t('请不要在图片链接中使用特殊字符')));
-	$socialweibo = new Typecho_Widget_Helper_Form_Element_Text('socialweibo', NULL, NULL, _t('输入微博链接'), _t('在这里输入微博链接,支持 http:// 或 https:// 或 //'));
+    $socialweibo = new Typecho_Widget_Helper_Form_Element_Text('socialweibo', NULL, NULL, _t('输入微博链接'), _t('在这里输入微博链接,支持 http:// 或 https:// 或 //'));
     $form->addInput($socialweibo->addRule('xssCheck', _t('请不要在链接中使用特殊字符')));
     $socialtwitter = new Typecho_Widget_Helper_Form_Element_Text('socialtwitter', NULL, NULL, _t('输入Twitter链接'), _t('在这里输入twitter链接,支持 http:// 或 https:// 或 //'));
     $form->addInput($socialtwitter->addRule('xssCheck', _t('请不要在链接中使用特殊字符')));
-	$socialfacebook = new Typecho_Widget_Helper_Form_Element_Text('socialfacebook', NULL, NULL, _t('输入Facebook链接'), _t('在这里输入Facebook链接,支持 http:// 或 https:// 或 //'));
+    $socialfacebook = new Typecho_Widget_Helper_Form_Element_Text('socialfacebook', NULL, NULL, _t('输入Facebook链接'), _t('在这里输入Facebook链接,支持 http:// 或 https:// 或 //'));
     $form->addInput($socialfacebook->addRule('xssCheck', _t('请不要在链接中使用特殊字符')));
-	$socialrss = new Typecho_Widget_Helper_Form_Element_Text('socialrss', NULL, NULL, _t('输入RSS链接'), _t('在这里输入rss链接,留空不输出，站点原版请输入off,支持 http:// 或 https:// 或 //'));
+    $socialrss = new Typecho_Widget_Helper_Form_Element_Text('socialrss', NULL, NULL, _t('输入RSS链接'), _t('在这里输入rss链接,留空不输出，站点原版请输入off,支持 http:// 或 https:// 或 //'));
     $form->addInput($socialrss->addRule('xssCheck', _t('请不要在链接中使用特殊字符')));
-		
-	//图片CDN
+        
+    //图片CDN
     $srcAddress = new Typecho_Widget_Helper_Form_Element_Text('src_add', NULL, NULL, _t('图片CDN替换前地址'), _t('即你的附件存放链接，一般为http://www.yourblog.com/usr/uploads/'));
     $form->addInput($srcAddress->addRule('xssCheck', _t('请不要在链接中使用特殊字符')));
     $cdnAddress = new Typecho_Widget_Helper_Form_Element_Text('cdn_add', NULL, NULL, _t('图片CDN替换后地址'), _t('即你的七牛云存储域名，一般为http://yourblog.qiniudn.com/，可能也支持其他有镜像功能的CDN服务'));
     $form->addInput($cdnAddress->addRule('xssCheck', _t('请不要在链接中使用特殊字符')));
     $default_thumb = new Typecho_Widget_Helper_Form_Element_Text('default_thumb', NULL, '', _t('默认缩略图'),_t('文章没有图片时的默认缩略图，留空则无，一般为http://www.yourblog.com/image.png'));
     $form->addInput($default_thumb->addRule('xssCheck', _t('请不要在链接中使用特殊字符')));
-	
-	
+    
+    
     $ad_postend = new Typecho_Widget_Helper_Form_Element_Text('ad_postend', NULL, NULL, _t('文章尾部广告图片链接'), _t('图片链接,支持 http:// 或 https:// 或 //'));
     $form->addInput($ad_postend->addRule('xssCheck', _t('请不要在图片链接中使用特殊字符')));
-	$ad_sidebar = new Typecho_Widget_Helper_Form_Element_Text('ad_sidebar', NULL, NULL, _t('侧边栏广告'), _t('自定义广告代码'));
+    $ad_sidebar = new Typecho_Widget_Helper_Form_Element_Text('ad_sidebar', NULL, NULL, _t('侧边栏广告'), _t('自定义广告代码'));
     $form->addInput($ad_sidebar);
 
-	
+    
 
 }
 
@@ -144,7 +144,7 @@ $defaults = array(
 'before' => '',
 'after' => '',
 'xformat' => '<a class="list-group-item visible-lg" title="{title}" href="{permalink}" rel="bookmark"><i class="fa  fa-book"></i> {title}</a> 
-	<a class="list-group-item visible-md" title="{title}" href="{permalink}" rel="bookmark"><i class="fa  fa-book"></i> {title}</a>'
+    <a class="list-group-item visible-md" title="{title}" href="{permalink}" rel="bookmark"><i class="fa  fa-book"></i> {title}</a>'
 );
 $time = time() - (24 * 60 * 60 * $days);
 $db = Typecho_Db::get();
@@ -169,7 +169,7 @@ $defaults = array(
 'before' => '',
 'after' => '',
 'xformat' => '<a class="list-group-item visible-lg" title="{title}" href="{permalink}" rel="bookmark"><i class="fa  fa-book"></i> {title}</a> 
-	<a class="list-group-item visible-md" title="{title}" href="{permalink}" rel="bookmark"><i class="fa  fa-book"></i> {title}</a>'
+    <a class="list-group-item visible-md" title="{title}" href="{permalink}" rel="bookmark"><i class="fa  fa-book"></i> {title}</a>'
 );
 $db = Typecho_Db::get();
  
@@ -195,25 +195,26 @@ function showThumb($obj,$size=null,$link=false){
     $thumb = '';
     $options = Typecho_Widget::widget('Widget_Options');
     $attach = $obj->attachments(1)->attachment;
-    if (isset($attach->isImage) && $attach->isImage == 1){
-        $thumb = $attach->url;
-        if(!empty($options->src_add) && !empty($options->cdn_add)){
-            $thumb = str_ireplace($options->src_add,$options->cdn_add,$thumb);
-        }
-    }elseif(isset($matches[1][0])){
+    // if (isset($attach->isImage) && $attach->isImage == 1){
+    //     $thumb = $attach->url;
+    //     if(!empty($options->src_add) && !empty($options->cdn_add)){
+    //         $thumb = str_ireplace($options->src_add,$options->cdn_add,$thumb);
+    //     }
+    // }else
+    if(isset($matches[1][0])){
         $thumb = $matches[1][0];
         if(!empty($options->src_add) && !empty($options->cdn_add)){
             $thumb = str_ireplace($options->src_add,$options->cdn_add,$thumb);
         }
     }
     if(empty($thumb) && empty($options->default_thumb)){
-		$thumb= $options->themeUrl .'/images/thumb/' . rand(1, 20) . '.jpg';
-		//去掉下面4行双斜杠 启用BING美图随机缩略图
-		$str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx='.rand(1, 30).'&n=1');
+        $thumb= $options->themeUrl .'/images/thumb/' . rand(1, 20) . '.jpg';
+        //去掉下面4行双斜杠 启用BING美图随机缩略图
+        $str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx='.rand(1, 30).'&n=1');
         $array = json_decode($str);
-		$imgurl = $array->{"images"}[0]->{"urlbase"};
+        $imgurl = $array->{"images"}[0]->{"urlbase"};
         $thumb = '//i'.rand(0, 2).'.wp.com/cn.bing.com'.$imgurl.'_1920x1080.jpg?resize=220,150';
-		
+        
         return $thumb;
     }else{
         $thumb = empty($thumb) ? $options->default_thumb : $thumb;
