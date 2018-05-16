1d1572c3a60704804041cc62df832e679af2dbcd
冯誉信 <ninvfeng@qq.com>
2018-05-16 15:00:58 +0800

    test

--- usr/themes/Kratos/post.php
+++ usr/themes/Kratos/post.php
@@ -9,7 +9,6 @@
 <?php $this->need('header.php'); ?>
 	<link href="https://cdn.bootcss.com/prism/1.14.0/themes/prism.min.css" rel="stylesheet">
 	<script src="https://cdn.bootcss.com/prism/1.14.0/prism.min.js"></script>
-	<script src="https://cdn.bootcss.com/prism/1.14.0/components/prism-php-extras.min.js"></script>
 	<script src="https://cdn.bootcss.com/prism/1.14.0/components/prism-php.min.js"></script>
 	<div class="container">
 		<div class="row">
