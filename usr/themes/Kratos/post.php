c3401d948160e581138b76d72a27d56ba7d11d6d
冯誉信 <ninvfeng@qq.com>
2018-05-16 15:11:24 +0800

    theme

--- usr/themes/Kratos/post.php
+++ usr/themes/Kratos/post.php
@@ -8,8 +8,9 @@
  if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 	<link href="https://cdn.bootcss.com/prism/1.14.0/themes/prism.min.css" rel="stylesheet">
+	<link href="https://cdn.bootcss.com/prism/1.14.0/themes/prism-okaidia.min.css" rel="stylesheet">
 	<script src="https://cdn.bootcss.com/prism/1.14.0/prism.min.js"></script>
-	<script src="https://cdn.bootcss.com/prism/1.14.0/components/prism-php.min.js"></script>
+	<script src="https://cdn.bootcss.com/prism/9000.0.1/components/prism-php-extras.min.js"></script>
 	<div class="container">
 		<div class="row">
 			<?php if ($this->options->sidebarlr == 'left_side'): ?>
